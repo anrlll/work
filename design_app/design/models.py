8ab11a024f7a27cf3bb386b0a6ea6a621007c13d
anrlll <103550372+anrlll@users.noreply.github.com>
2024-01-16 05:02:22 +0900

    fix_gadget_before

--- design_app/design/models.py
+++ design_app/design/models.py
@@ -31,10 +31,13 @@ class Photo(models.Model):
     title = models.CharField(max_length=150)
     comment = models.TextField(blank=True)
     image = models.ImageField(upload_to="photos") #画像ファイル
+    description = models.TextField(blank=True, null=True)#ガジェットの説明
     user = models.ForeignKey(User, on_delete=models.CASCADE)#Userインスタンスが削除された場合、紐付けされている投稿を削除する挙動を定義
     category = models.ForeignKey(Category, on_delete=models.PROTECT)#作成したcategoryモデルを使用して、フィールド作成
     top = models.ForeignKey(Top, on_delete=models.PROTECT)#メイン画像判定
+    order = models.PositiveIntegerField(default=0,blank=True)
     created_at = models.DateTimeField(auto_now=True)
+    carousel = models.PositiveIntegerField(default=0)
     
     def __str__(self):
         return self.title
--- design_app/design/views.py
+++ design_app/design/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Photo
+from .models import Photo,Category
 
 # Create your views here.
 
@@ -7,11 +7,21 @@ def gadget(request):
     return render(request, "design/gadget_home.html")
 
 def g_before(request):
-    photos = Photo.objects.filter(top="1").order_by("created_at")
-    return render(request, "design/gadget_before.html",{"photos":photos})
+    photos = Photo.objects.filter(top="1").order_by("order")#Photoの情報一部を取得
+    categories = Photo.objects.filter(top="2")#Photoのsubを取得
+
+    # filtered_photos = [photo for photo in photos if categories.filter(category=photo.category).exists()]
+    
+    return render(request, "design/gadget_before.html",{"photos":photos, "categories":categories})
+
+
+
+
+
+
 
 def g_after(request):
-    photos = Photo.objects.filter(top="1").order_by("created_at")
+    photos = Photo.objects.filter(top="1").order_by("order")
     return render(request, "design/gadget_after.html",{"photos":photos})
 
 
