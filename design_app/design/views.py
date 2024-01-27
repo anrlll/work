f115a0346235a5db679841e5dda11a77e97e24f0
anrlll <103550372+anrlll@users.noreply.github.com>
2024-01-27 19:09:12 +0900

    change_carousel_color

--- design_app/design/views.py
+++ design_app/design/views.py
@@ -21,7 +21,9 @@ def g_after(request):
 
 def g_after_detail(request, pk):
     photo = get_object_or_404(Photo, pk=pk)
-    return render(request, "design/gadget_detail.html",{'photo': photo})
+    photos = Photo.objects.filter(top="1").order_by("order")#Photoの情報一部を取得
+    categories = Photo.objects.filter(top="2")#Photoのsubを取得
+    return render(request, "design/gadget_detail.html",{'photo': photo, "photos":photos, "categories":categories})
 
 
 
